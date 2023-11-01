a07a308ee5f15bd550e39631217555daa416b0a9
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-11-01 12:43:57 +0000

    Azure Key Vault Secrets operations in .NET

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace dotnetcore
 {
@@ -6,7 +9,33 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string secretName = "MySecret";
+            string keyVaultName = "vault-no-protect";
+            var kvUri = "https://vault-no-protect.vault.azure.net/";
+
+            SecretClientOptions options = new() {
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(), options);
+            KeyVaultSecret secret = client.GetSecret(secretName);
+
+            Console.WriteLine("GetSecret: " + secret.Value);
+            Console.Write("Enter Secret > ");
+
+            string secretValue = Console.ReadLine();
+            client.SetSecret(secretName, secretValue);
+            Console.WriteLine("SetSecret:");
+            Console.Write(" Key: " + secretName);
+            Console.WriteLine(" Value: " + secret.Value);
+
+            client.StartDeleteSecret(secretName);
+            Console.WriteLine("StartDeleteSecret: " + keyVaultName);
         }
     }
 }
